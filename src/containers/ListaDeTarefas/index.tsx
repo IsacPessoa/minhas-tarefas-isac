06584830dea9c089baffe4fdf94556186cf13c91
Isac Pessoa <isacdavidpessoa@hotmail.com>
2024-03-25 18:06:27 -0300

    criando o estado de troca de botões

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver aula 5 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar conta de internet',
+    descricao: 'Baixar fatura no e-mail',
+    prioridade: 'urgente',
+    status: 'concluida'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino A',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
